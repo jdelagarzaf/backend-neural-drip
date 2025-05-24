4b006734f07bcad0da74c23ea3a92a6cb45f5a74
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-05-24 15:22:47 -0600

    add routes for tables

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,21 +7,14 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+// Routes
+app.use('/api/dim-tienda', require('./routes/dimTienda'));
+app.use('/api/meta-venta', require('./routes/metaVenta'));
+app.use('/api/venta', require('./routes/venta'));
 
-app.get('/api/meta_venta', async (req, res) => {
-  const { rows } = await pool.query('SELECT * FROM meta_venta');
-  res.json(rows);
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
 });
-
-app.get('/api/db-check', async (req, res) => {
-  const result = await pool.query('SELECT version()');
-  res.send(result.rows[0]);
-});
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
